60e6cee50369af2639244d4b011ab4d11d270428
0xstacker <oladotuntemitopeakorede@gmail.com>
2025-02-26 10:23:32 +0100

    update

--- src/ERC721Collection.sol
+++ src/ERC721Collection.sol
@@ -3,14 +3,13 @@ pragma solidity 0.8.25;
 import {ERC721} from "@openzeppelin/token/ERC721/ERC721.sol";
 import {IERC721Collection} from "./IERC721Collection.sol";
 import {MerkleProof} from "@openzeppelin/utils/cryptography/MerkleProof.sol";
-import {Phase} from "./PhaseLib.sol";
+import {ReentrancyGuard} from "@openzeppelin/utils/ReentrancyGuard.sol";
 
 /**
 * @dev Implementation of an ERC721 drop.
-
 */
 
-contract Drop is ERC721{
+contract Drop is ERC721, IERC721Collection, ReentrancyGuard{
     
     uint8 constant PHASELIMIT = 5;
     
@@ -22,30 +21,32 @@ contract Drop is ERC721{
 
     bool public paused;
     
-    uint64 public MAX_SUPPLY;
+    uint64 public maxSupply;
+   
+    uint64 private tokenId;
     
     uint64 public totalMinted;
 
     uint private immutable price;
     
-    uint private tokenId;
-    
-    uint private royalty;
+    // uint private constant royalty;
+
+    string baseURI;
 
     
 
 /*** @dev Platform mint fee */
-    uint public mintFee;
+    uint public immutable mintFee;
     
-    Phase.PublicMint internal _publicMint;
+    PublicMint internal _publicMint;
 
     // Sequential phase identities, 0 represents the public minting phase.
     uint8 internal phaseIds;
-    Phase.PresalePhase[5] public mintPhases;
+    PresalePhase[5] public mintPhases;
 
     mapping(uint8 => bool) public phaseCheck;
  
-    Phase.PresalePhase[] internal _returnablePhases;
+    PresalePhase[] internal _returnablePhases;
     using MerkleProof for bytes32[];
 
 
@@ -72,18 +73,23 @@ contract Drop is ERC721{
     uint _price,
     uint8 _maxPerWallet,
     address _owner,
-    address _feeReceipient) ERC721(_name, _symbol){
-        MAX_SUPPLY = _maxSupply;
+    string memory _baseUri,
+    address _feeReceipient) ERC721(_name, _symbol) ReentrancyGuard(){
+        maxSupply = _maxSupply;
         price = _price;
         // Ensure that owner is an EOA and not zero address
         require(_owner.code.length == 0 && _owner != address(0), "Invalid Adress");
         owner = _owner;
+        if (_feeReceipient == address(0)){
+            revert ERC721InvalidReceiver(address(0));
+        }
         _FEE_RECEIPIENT = _feeReceipient;
         mintFee = _mintFee;
         _publicMint.startTime = block.timestamp + _startTime;
         _publicMint.endTime = block.timestamp + _startTime + _duration;
         _publicMint.price = _price;
         _publicMint.maxPerWallet = _maxPerWallet;
+        baseURI = _baseUri;
     }
 
     receive() external payable { }
@@ -103,6 +109,7 @@ contract Drop is ERC721{
         if(_owner != msg.sender){
             revert NotOwner();
         }
+        _;
     }
 
     // Block minting unless phase is active
@@ -143,16 +150,11 @@ contract Drop is ERC721{
     }
 
     /**
-    * @dev Public minting function.
-    * @param _amount is the amount of nfts to mint
-    * @param _to is the address to mint the tokens to
-    * @notice can only mint when public sale has started and the minting process is not paused by the creator
-    * @notice minting is limited to the maximum amounts allowed on the public mint phase.
-    */
-
-    function mintPublic(uint _amount, address _to) external payable phaseActive(0) limit(_to, _amount, 0) isPaused{
+     * @dev see {IERC721Collection-mintPublic}
+     */
+    function mintPublic(uint _amount, address _to) external payable phaseActive(0) limit(_to, _amount, 0) isPaused nonReentrant{
         if (!_canMint(_amount)){
-            revert SoldOut(MAX_SUPPLY);
+            revert SoldOut(maxSupply);
         }
         uint totalCost = _getCost(0, _amount);
         if(msg.value < totalCost){
@@ -166,17 +168,15 @@ contract Drop is ERC721{
 
     
     /**
-    * @dev adds new presale phase for contract
-    * @param _phase is the new phase to be added
-    * @notice phases are identified sequentially using numbers, starting from 1.
-    */
-    function addPresalePhase(Phase.PresalePhaseIn calldata _phase) external onlyCreator{
+     * @dev see {IERC721Collection-addPresalePhase}
+     */
+    function addPresalePhase(PresalePhaseIn calldata _phase) external onlyCreator{
         if (phaseIds == PHASELIMIT){
             revert PhaseLimitExceeded(PHASELIMIT);  
         }
         uint8 phaseId = phaseIds + 1;
 
-        Phase.PresalePhase memory phase = Phase.PresalePhase({
+        PresalePhase memory phase = PresalePhase({
             name: _phase.name,
             startTime: block.timestamp + _phase.startTime,
             endTime: block.timestamp + _phase.endTime,
@@ -198,65 +198,40 @@ contract Drop is ERC721{
     }
 
     /**
-    * @dev Remove presale phase
-    * @param _phaseId is the identifier for the phase being removed
-    */
-
-    function removePhase(uint8 _phaseId) external onlyCreator{
-        if (!phaseCheck[_phaseId]){
-            revert InvalidPhase(_phaseId);
-        }
-
-        Phase.PresalePhase[] memory returnablePhases = _returnablePhases;
-        delete _returnablePhases;
-        for (uint8 i; i < returnablePhases.length; i++){
-            if (returnablePhases[i].phaseId != _phaseId){
-                _returnablePhases.push(returnablePhases[i]);
-            }
-        }
-
-        delete mintPhases[_phaseId];
-        phaseCheck[_phaseId] = false;
-        emit RemovePresalePhase(mintPhases[_phaseId].name, _phaseId);
-    }
-
+     * @dev see {IERC721Collection-reduceSupply}
+     */
     function reduceSupply(uint64 _newSupply) external onlyCreator{
-        if(_newSupply >= totalMinted || _newSupply < totalMinted){
+        if(_newSupply > totalMinted || _newSupply > maxSupply){
             revert InvalidSupplyConfig();
         }
-        MAX_SUPPLY = _newSupply;
+        maxSupply = _newSupply;
+        emit SupplyReduced(_newSupply);
     }
 
     // getter for presale phase data
-    function getPresaleData() external view returns(Phase.PresalePhase[] memory){
+    function getPresaleData() external view returns(PresalePhase[] memory){
         return _returnablePhases;
     }
 
     // getter for public mint data
-    function getPublicMintData() external view returns(Phase.PublicMint memory){
+    function getPublicMintData() external view returns(PublicMint memory){
         return _publicMint;
     }
-
-    /**
-    * @dev Allows creator to airdrop NFTs to an account
-    * @param _to is the address of the receipeient
-    * @param _amount is the amount of NFTs to be airdropped
-    * Ensures amount of tokens to be minted does not exceed MAX_SUPPLY*/
-
+    
+    /***
+     * @dev see {IERC721Collection-airdrop}
+     */
     function airdrop(address _to, uint _amount) external onlyCreator{
         if(!_canMint(_amount)){
-            revert SoldOut(MAX_SUPPLY);
+            revert SoldOut(maxSupply);
         }
         _mintNft(_to, _amount);
         emit Airdrop(_to, tokenId, _amount);
     }
-    
-    /**
-    * @dev Allows the creator to airdrop NFT to multiple addresses at once.
-    * @param _receipients is the list of accounts to mint NFT for.
-    * @param _amountPerAddress is the amount of tokens to be minted per addresses.
-    * Ensures total amount of NFT to be minted does not exceed MAX_SUPPLY.
-    * */
+
+    /***
+     * @dev see {IERC721Collection-batchAirdrop}
+     */
     function batchAirdrop(address[] calldata _receipients, uint _amountPerAddress) external onlyCreator{
         if(_receipients.length > BATCH_MINT_LIMIT){
             revert AmountTooHigh();
@@ -264,7 +239,7 @@ contract Drop is ERC721{
 
         uint totalAmount = _amountPerAddress * _receipients.length;
         if (!_canMint(totalAmount)){
-            revert SoldOut(MAX_SUPPLY);
+            revert SoldOut(maxSupply);
         }
         for(uint i; i < _receipients.length; i++){
             _mintNft(_receipients[i], _amountPerAddress);
@@ -285,7 +260,7 @@ contract Drop is ERC721{
     }
 
     // Withdraw funds from contract
-    function withdraw(uint _amount) external onlyCreator{
+    function withdraw(uint _amount) external onlyCreator nonReentrant{
         if (address(this).balance < _amount){
             revert InsufficientFunds(_amount);
         }
@@ -296,25 +271,16 @@ contract Drop is ERC721{
         emit WithdrawFunds(_amount);
     }
 
-    /**
-    * @dev Check the whitelist status of an account based on merkle proof.
-    * @param _proof is a merkle proof to check for verification.
-    * @param _amount is the amount of tokens to be minted.
-    * @param _phaseId is the presale phase the user is attempting to mint for.
-    * @notice If phase is not active, function reverts.
-    * @notice If amount exceeds the maximum allowed to be minted per walllet, function reverts.
-    */
-
-    function whitelistMint(bytes32[] memory _proof, uint8 _amount, uint8 _phaseId) external payable phaseActive(_phaseId) limit(msg.sender, _amount, _phaseId) isPaused{
+    /***
+     * @dev see {IERC721Collection-whitelistMint}
+     */
+    function whitelistMint(bytes32[] memory _proof, uint8 _amount, uint8 _phaseId) external payable phaseActive(_phaseId) limit(msg.sender, _amount, _phaseId) nonReentrant isPaused{
         if (!phaseCheck[_phaseId]){
             revert InvalidPhase(_phaseId);
         }
-        // PresalePhase memory phase = phases[_phaseId];
         if (!_canMint(_amount)){
-            revert SoldOut(MAX_SUPPLY);
+            revert SoldOut(maxSupply);
         }
-        
-        // get mint cost
         uint totalCost = _getCost(_phaseId, _amount);
         if (msg.value < totalCost){
             revert InsufficientFunds(totalCost);
@@ -329,9 +295,8 @@ contract Drop is ERC721{
         _payCreator(_amount);
     }
 
-
     /**
-    * @dev Allows owner to burn their nft
+    * @dev see {ERC721-_burn}
     */
     function burn(uint _tokenId) external tokenOwner(_tokenId){
         _burn(_tokenId);
@@ -339,20 +304,33 @@ contract Drop is ERC721{
 
       // total supply
     function supply() external view returns(uint){
-        return MAX_SUPPLY;
+        return maxSupply;
     }
 
-    function contractOwner() external view rerturns(address){
+    function setBaseURI(string memory _uri) external onlyCreator{
+        baseURI = _uri;
+    }   
+
+    /***
+     * @dev see {IERC721Collection-contractOwner}
+     */
+    function contractOwner() external view returns(address){
         return owner;
     }
- 
+    /***
+     * @dev see {ERC721-_baseURI}
+     */
+    function _baseURI() internal view override returns(string memory){
+        return baseURI;
+    }
+
     /**
     * @dev Checks if a certain amount of token can be minted. 
     * @param _amount is the amount of tokens to be minted.
     * @notice Ensures that minting _amount tokens does not cause the total minted tokens to exceed max supply.
     */
     function _canMint(uint _amount) internal view returns (bool){
-        if (totalMinted + _amount > MAX_SUPPLY){
+        if (totalMinted + _amount > maxSupply){
             return false;
         } else{
             return true;
@@ -377,6 +355,7 @@ contract Drop is ERC721{
 
     /***
      * @dev Payout platform fee
+     * @param _amount is the amount of ether to be paid to the platform as fee.
      */
 
     function _payoutPlatformFee(uint _amount) internal{
@@ -387,6 +366,10 @@ contract Drop is ERC721{
         }
     }
 
+    /**
+     * @dev pay creator
+     * @param _amount is the amount of ether to be paid to creator after paying platform fee.
+     */
     function _payCreator(uint _amount) internal{
         uint fee = mintFee * _amount;
         (bool success, ) = payable(owner).call{value: msg.value - fee}("");
@@ -396,16 +379,20 @@ contract Drop is ERC721{
     }
     
     /**
-     * @dev Safe minting function that will mint n amount of tokens to an address.
+     * @dev Mint tokens to an address.
      * @param _to is the address of the receipient.
      * @param _amount is the amount of tokens to be minted.
     */
 
     function _mintNft(address _to, uint _amount) internal isPaused {  
+        uint64 tokenIdInc;
+        uint64 totalMintedInc;
         for(uint i; i < _amount; i++){
-            tokenId += 1;
-            totalMinted += 1;
+            tokenIdInc += 1;
+            totalMintedInc += 1;
             _safeMint(_to, tokenId);
         }
+        tokenId += tokenIdInc;
+        totalMinted += totalMintedInc; 
     }
 }
--- src/IERC721Collection.sol
+++ src/IERC721Collection.sol
@@ -3,7 +3,40 @@ pragma solidity 0.8.25;
 
 interface IERC721Collection{
 
-    error NotWhit~elisted(address _address);
+    struct PresalePhaseIn{
+        uint8 maxPerAddress;
+        string name;
+        uint price;
+        uint startTime;
+        uint endTime;
+        bytes32 merkleRoot;
+    }
+
+    /**
+    * @dev Holds the details of the public/general mint phase
+    */
+    struct PublicMint{
+        uint startTime;
+        uint endTime;
+        uint price;
+        uint maxPerWallet;
+    }
+
+    /**
+     * @dev Holds the details of a presale phase
+     */
+    struct PresalePhase{
+        uint8 maxPerAddress;
+        string name;
+        uint price;
+        uint startTime;
+        uint endTime;
+        bytes32 merkleRoot;
+        uint phaseId;
+    }
+
+
+    error NotWhitelisted(address _address);
     error InsufficientFunds(uint _cost);
     error SoldOut(uint maxSupply);
     error InvalidPhase(uint8 _phaseId);
@@ -31,4 +64,59 @@ interface IERC721Collection{
     event SalePaused();
     event PublicMintDisabled();
 
+    /**
+    * @dev Public minting function.
+    * @param _amount is the amount of nfts to mint
+    * @param _to is the address to mint the tokens to
+    * @notice can only mint when public sale has started and the minting process is not paused by the creator
+    * @notice minting is limited to the maximum amounts allowed on the public mint phase.
+    */
+
+    function mintPublic(uint _amount, address _to) external payable;
+
+    /**
+    * @dev adds new presale phase for contract
+    * @param _phase is the new phase to be added
+    * @notice phases are identified sequentially using numbers, starting from 1.
+    */
+    function addPresalePhase(PresalePhaseIn calldata _phase) external;
+
+    /***
+     * @dev Reduce the collection supply
+     * @param _newSupply is the new supply to be set
+     * Ensures that the new supply is not less than the total minted tokens
+     * 
+     */
+    function reduceSupply(uint64 _newSupply) external;
+
+    /**
+    * @dev Allows creator to airdrop NFTs to an account
+    * @param _to is the address of the receipeient
+    * @param _amount is the amount of NFTs to be airdropped
+    * Ensures amount of tokens to be minted does not exceed MAX_SUPPLY*/
+
+    function airdrop(address _to, uint _amount) external;
+
+    /**
+    * @dev Allows the creator to airdrop NFT to multiple addresses at once.
+    * @param _receipients is the list of accounts to mint NFT for.
+    * @param _amountPerAddress is the amount of tokens to be minted per addresses.
+    * Ensures total amount of NFT to be minted does not exceed MAX_SUPPLY.
+    * */
+    function batchAirdrop(address[] calldata _receipients, uint _amountPerAddress) external;
+
+    /**
+    * @dev Check the whitelist status of an account based on merkle proof.
+    * @param _proof is a merkle proof to check for verification.
+    * @param _amount is the amount of tokens to be minted.
+    * @param _phaseId is the presale phase the user is attempting to mint for.
+    * @notice If phase is not active, function reverts.
+    * @notice If amount exceeds the maximum allowed to be minted per walllet, function reverts.
+    */
+    function whitelistMint(bytes32[] memory _proof, uint8 _amount, uint8 _phaseId) external payable;
+
+    /***'
+     * @dev Get the creator address
+     */
+    function contractOwner() external view returns(address);
 }
